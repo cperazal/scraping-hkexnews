8e80ad4b9ac4d630494b9420a17bae0a2cc1d4d3
Carlos Peraza <carlosperaza17@gmail.com>
2022-02-05 00:30:09 -0600

    Initial commit

---
+++ hkexnews/spiders/hkexnews_spider.py
@@ -0,0 +1,37 @@
+import scrapy
+from scrapy.http import FormRequest
+
+class HkexnewsSpiderSpider(scrapy.Spider):
+    name = 'hkexnews_spider'
+    allowed_domains = ['hkexnews.hk']
+    start_urls = ['https://www.hkexnews.hk/sdw/search/searchsdw.aspx']
+
+    def parse(self, response):
+
+        data = {
+            '__EVENTTARGET': 'btnSearch',
+            '__VIEWSTATE': response.xpath('//*[@name="__VIEWSTATE"]/@value').extract_first(),
+            '__VIEWSTATEGENERATOR': response.xpath('//*[@name="__VIEWSTATEGENERATOR"]/@value').extract_first(),
+            'today': '20220205',
+            'sortBy': 'shareholding',
+            'sortDirection': 'desc',
+            'txtShareholdingDate': '2022/02/04',
+            'txtStockCode': '00001'
+        }
+
+        return [FormRequest("https://www.hkexnews.hk/sdw/search/searchsdw.aspx",
+                               formdata=data,
+                               callback=self.parse_result)]
+
+
+    def parse_result(self, response):
+        table_rows = response.xpath('//*[@id="pnlResultNormal"]//table/tbody/tr')
+        for row in table_rows:
+            yield {
+                'participant_id': row.xpath('.//td[1]/div[2]//text()').extract_first(),
+                'participant_name': row.xpath('.//td[2]/div[2]//text()').extract_first(),
+                'address': row.xpath('.//td[3]/div[2]//text()').extract_first(),
+                'share_holding': row.xpath('.//td[4]/div[2]//text()').extract_first(),
+                'percent_issued_shares': row.xpath('.//td[5]/div[2]//text()').extract_first()
+            }
+
